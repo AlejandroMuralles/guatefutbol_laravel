e4bbcd38dbd7dda68cc99deababf9d266694f7ed
Alejandro Muralles <alejandromuralles57@gmail.com>
2018-01-02 07:07:06 -0600

    minify bootstrap y styles blue

--- resources/views/layouts/publico.blade.php
+++ resources/views/layouts/publico.blade.php
@@ -5,8 +5,8 @@
         <title>@yield('title')</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <!-- Bootstrap 3.3.2 -->
-        <link href="{{ asset('assets/public/css/plugins/bootstrap/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets/public/css/style-blue.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/public/css/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/public/css/style-blue.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- Font Awesome Icons -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <!-- Ionicons -->
