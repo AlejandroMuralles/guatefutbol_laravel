c437a6dc6180e5308147d5da596550e278bdf468
Alejandro Muralles <alejandromuralles57@gmail.com>
2018-12-12 14:36:30 -0600

    agregar campos de menu_app a campeonato

--- app/App/Managers/CampeonatoManager.php
+++ app/App/Managers/CampeonatoManager.php
@@ -32,7 +32,13 @@ class CampeonatoManager extends BaseManager
 	{
 		$data['grupos'] = isset($data['grupos']) ? 1 : 0;
 		$data['actual'] = isset($data['actual']) ? 1 : 0;
-		$data['mostrar_app'] = isset($data['mostrar_app']) ? 1 : 0;
+        $data['mostrar_app'] = isset($data['mostrar_app']) ? 1 : 0;
+        $data['menu_app_calendario'] = isset($data['menu_app_calendario']) ? 1 : 0;
+        $data['menu_app_posiciones'] = isset($data['menu_app_posiciones']) ? 1 : 0;
+        $data['menu_app_tala_acumulada'] = isset($data['menu_app_tala_acumulada']) ? 1 : 0;
+        $data['menu_app_goleadores'] = isset($data['menu_app_goleadores']) ? 1 : 0;
+        $data['menu_app_porteros'] = isset($data['menu_app_porteros']) ? 1 : 0;
+        $data['menu_app_plantilla'] = isset($data['menu_app_plantilla']) ? 1 : 0;
 		return $data;
 	}
 
