ec6829afc1f6e2b55f281a10291c34ff6308bf1a
Alejandro Muralles <alejandromuralles57@gmail.com>
2020-10-10 15:58:17 -0600

    fixing partido para ios

--- app/Http/Controllers/ApiV2Controller.php
+++ app/Http/Controllers/ApiV2Controller.php
@@ -758,10 +758,10 @@ class ApiV2Controller extends BaseController {
 		$p['hora'] = date('H:i',strtotime($partido->fecha));
 		$p['equipo_local'] = $partido->equipo_local->nombre_corto;
 		$p['logo_equipo_local'] = $partido->equipo_local->logo;
-		$p['goles_equipo_local'] = $partido->goles_local;
+		$p['goles_equipo_local'] = $partido->goles_local ?? 0;
 		$p['equipo_visita'] = $partido->equipo_visita->nombre_corto;
 		$p['logo_equipo_visita'] = $partido->equipo_visita->logo;
-		$p['goles_equipo_visita'] = $partido->goles_visita;
+		$p['goles_equipo_visita'] = $partido->goles_visita ?? 0;
 		$p['estado'] = $partido->estado;
 		return $p;
 	}
